6586fffe2d47ea8f4c686726b93018577dc89d7e
kristen-howton <kristen.howton@gmail.com>
2020-05-01 15:12:07 -0500

    delete works

--- src/components/recipe/Recipe.js
+++ src/components/recipe/Recipe.js
@@ -1,8 +1,21 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import { Button } from "reactstrap"
+import { RecipeContext } from "./RecipeProvider"
 
-export const Recipe = ({ recipe }) => (
-    <section className="recipe">
+export const Recipe = ({ recipe }) => { 
+
+const { recipes, deleteRecipe } = useContext(RecipeContext)
+const [e, setRecipes] = useState({recipe: { id: 0 }, name: null, instruction: null } )  
+
+return (
+
+    < section className="recipe" >
         <h3 className="recipe">{recipe.name}</h3>
         <div className="recipe__instructions">{recipe.instructions}</div>
-    </section>
+        <Button color="danger" onClick={() => {
+            deleteRecipe(recipe.id)
+            setRecipes(recipes)
+        }}>Delete 🤢</Button>
+    </section >
 )
+}
