15a2e21105e277c4eca4aca01dbb4de84a0d119a
kristen-howton <kristen.howton@gmail.com>
2020-05-02 11:32:28 -0500

    edit button shows, now to make it functional

--- src/components/recipe/Recipe.js
+++ src/components/recipe/Recipe.js
@@ -2,18 +2,19 @@ import React, { useContext } from "react"
 import { Button } from "reactstrap"
 import { RecipeContext } from "./RecipeProvider"
 
-export const Recipe = ({ recipe }) => { 
+export const Recipe = ({ recipe }) => {
 
-const { deleteRecipe } = useContext(RecipeContext)  
+    const { deleteRecipe, updateRecipe } = useContext(RecipeContext)
 
-return (
-
-    < section className="recipe" >
-        <h3 className="recipe">{recipe.name}</h3>
-        <div className="recipe__instructions">{recipe.instructions}</div>
-        <Button color="danger" onClick={() => {
-            deleteRecipe(recipe.id)
-        }}>Delete 🤢</Button>
-    </section >
-)
+    return (
+        < section className="recipe" >
+            <h3 className="recipe">{recipe.name}</h3>
+            <div className="recipe__instructions">{recipe.instructions}</div>
+            <Button color="info" onClick={() => {
+            updateRecipe(recipe.id)}}>Edit</Button>
+            <Button color="danger" onClick={() => {
+                deleteRecipe(recipe.id)
+            }}>Delete🤢</Button>
+        </section >
+    )
 }
