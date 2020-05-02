1c9245fae80a92e6e2cca9143e9b10b3303194fa
kristen-howton <kristen.howton@gmail.com>
2020-05-02 17:50:08 -0500

    edit button is functional

---
+++ src/components/recipe/EditRecipeForm.js
@@ -0,0 +1,70 @@
+import React, { useContext, useState } from "react"
+import { RecipeContext } from "./RecipeProvider"
+
+export const EditRecipeForm = ({ recipe, toggleEdit }) => {
+    const { updateRecipe } = useContext(RecipeContext)
+
+    // Separate state variable to track the recipe as it is edited
+    const [updatedRecipe, setRecipe] = useState(recipe)
+
+    /*
+        When changing a state object or array, always create a new one
+        and change state instead of modifying current one
+    */
+    const handleControlledInputChange = (event) => {
+        // Create a new copy of the recipe being edited
+        const newRecipe = Object.assign({}, updatedRecipe)
+
+        // Change the property value on the copy
+        newRecipe[event.target.name] = event.target.value
+
+        // Set the copy as the new state
+        setRecipe(newRecipe)
+    }
+
+    const editRecipe = () => {
+
+        updateRecipe({
+            id: updatedRecipe.id,
+            name: updatedRecipe.name,
+            instructions: updatedRecipe.instructions,
+            userId: parseInt(localStorage.getItem("recipe_user"))
+
+        })
+            .then(toggleEdit)
+    }
+
+
+    return (
+        <form className="recipeForm">
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Recipe name: </label>
+                    <input type="text" name="name" required autoFocus className="form-control"
+                        placeholder="Recipe name"
+                        defaultValue={recipe.name}
+                        onChange={handleControlledInputChange}
+                    />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="recipe">Recipe instructions: </label>
+                    <input type="text" name="instructions" required className="form-control"
+                        placeholder="Recipe insturctions"
+                        defaultValue={recipe.instructions}
+                        onChange={handleControlledInputChange}
+                    />
+                </div>
+            </fieldset>
+
+            <button type="submit" className="btn btn-primary"
+                onClick={evt => {
+                    evt.preventDefault()
+                    editRecipe()
+                }}>
+                Save Changes
+            </button>
+        </form>
+    )
+}
--- src/components/recipe/Recipe.js
+++ src/components/recipe/Recipe.js
@@ -1,20 +1,33 @@
-import React, { useContext } from "react"
-import { Button } from "reactstrap"
+import React, { useContext, useState } from "react"
+import { Button, Modal, ModalHeader, ModalBody } from "reactstrap"
 import { RecipeContext } from "./RecipeProvider"
+import { EditRecipeForm } from "./EditRecipeForm"
+
 
 export const Recipe = ({ recipe }) => {
 
-    const { deleteRecipe, updateRecipe } = useContext(RecipeContext)
+    const { deleteRecipe } = useContext(RecipeContext)
+    const [editModal, setEditModal] = useState(false)
+    const toggleEdit = () => setEditModal(!editModal)
 
     return (
         < section className="recipe" >
             <h3 className="recipe">{recipe.name}</h3>
             <div className="recipe__instructions">{recipe.instructions}</div>
             <Button color="info" onClick={() => {
-            updateRecipe(recipe.id)}}>Edit</Button>
+                toggleEdit()
+            }}>Edit</Button>
             <Button color="danger" onClick={() => {
                 deleteRecipe(recipe.id)
             }}>Delete🤢</Button>
+            <Modal isOpen={editModal} toggle={toggleEdit}>
+                <ModalHeader toggle={toggleEdit}>
+                    {recipe.name}
+                </ModalHeader>
+                <ModalBody>
+                    <EditRecipeForm key={recipe.id} toggleEdit={toggleEdit} recipe={recipe} />
+                </ModalBody>
+            </Modal>
         </section >
     )
 }
