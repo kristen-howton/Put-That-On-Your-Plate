c13fe03d7d47a542e35d1546c531170763065762
kristen-howton <kristen.howton@gmail.com>
2020-05-08 13:09:46 -0500

    user can click button and see only the favorite recipes

--- src/components/recipe/RecipeList.js
+++ src/components/recipe/RecipeList.js
@@ -2,42 +2,56 @@ import React, { useContext, useState, useEffect } from "react"
 import { RecipeContext } from "./RecipeProvider"
 import { Recipe } from "./Recipe"
 import { RecipeForm } from "./RecipeForm"
-import { Button, Modal, ModalBody, ModalHeader} from "reactstrap"
+import { Button, Modal, ModalBody, ModalHeader } from "reactstrap"
 import "./Recipe.css"
 
-export const RecipeList = ( {searchTerms, recipeType, activeUser} ) => {
+export const RecipeList = ({ searchTerms, recipeType, activeUser }) => {
     const { recipes } = useContext(RecipeContext)
 
     const [modal, setModal] = useState(false)
     const toggle = () => setModal(!modal)
+
+    const [favorite, setFavorite] = useState(false)
+    const toggleFavorites = () => setFavorite(!favorite)
+
     const [matchingRecipes, setMatchingRecipes] = useState([])
-    
+
     useEffect(
         () => {
             let filteredRecipes = recipes.filter(recipe => {
-                return parseInt(activeUser) === recipe.userId })
+                return parseInt(activeUser) === recipe.userId
+            })
 
-            if(searchTerms !== ""){
+            if (searchTerms !== "") {
                 filteredRecipes = filteredRecipes.filter(recipe => recipe.name.toLowerCase().includes(searchTerms))
             }
 
-            if(recipeType !== "0"){
+            if (recipeType !== "0") {
                 filteredRecipes = filteredRecipes.filter(recipe => recipe.recipeTypeId === parseInt(recipeType))
+            }
+
+            if (favorite) {
+                filteredRecipes = filteredRecipes.filter(recipe => recipe.isFavorite)
             }
 
             setMatchingRecipes(filteredRecipes)
 
-     },
-        [searchTerms, recipes, recipeType, activeUser]
+        },
+        [searchTerms, recipes, recipeType, activeUser, favorite]
     )
-    
+
     return (
         <>
-            <Button onClick={() => {
-                    // Show the recipe form
-                    toggle()
-                }}>Add Recipe</Button>
-           
+            <Button className="addRecipeBtn" onClick={() => {
+                // Show the recipe form
+                toggle()
+            }}>Add Recipe</Button>
+
+            <Button className="favoriteBtn" onClick={() => {
+                // Show the favorites
+                toggleFavorites()
+            }}>Show Favorites</Button>
+
             <div className="recipes">
                 {
                     matchingRecipes.map(rec => <Recipe key={rec.id} recipe={rec} />)
@@ -52,7 +66,7 @@ export const RecipeList = ( {searchTerms, recipeType, activeUser} ) => {
                     <RecipeForm toggler={toggle} />
                 </ModalBody>
             </Modal>
-            
+
         </>
     )
 }
